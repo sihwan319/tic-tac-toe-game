7b0f63cd52da52da14d59db1783622070a28ad02
sihwankim <sihwan319@gmail.com>
2023-02-14 11:09:47 +0900

    player enrollment form complete

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,39 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) { 
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1
     playerConfigOverLayElement.style.display = 'block';
     backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverLayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+
+    if(!enteredPlayername) { //enteredPlayername === ''
+        event.target.firstElementChild.classList.add('error');
+        errorOutputElement.textContent = 'Please enter a valid name!';
+        return;
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+
+    players[editedPlayer - 1].name = enteredPlayername;
+    // if(editedPlayer === 1 ) {
+    //     player[0].name = enteredPlayername;
+    // } else {
+    //     player[1].name = enteredPlayername;
+    // }
+
+    closePlayerConfig();
 
 }
